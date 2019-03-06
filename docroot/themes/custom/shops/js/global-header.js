a5900e692d2633c79b3b2eaf1ecc1b88d6376139
Ivan Berezhnov <ivan.berezhnov@icloud.com>
2019-03-06 22:23:01 +0200

    feat: add new styled elements

--- docroot/themes/custom/shops/js/global-header.js
+++ docroot/themes/custom/shops/js/global-header.js
@@ -8,17 +8,31 @@
   /**
    *
    */
-  Drupal.behaviors.shopDropdownBtn = {
-    attach: function (context, settings) {
+  // Drupal.behaviors.shopDropdownBtn = {
+  //   attach: function (context, settings) {
+  //
+  //     $('.dropbtn').once('shopDropdownBtn').touchstart(function () {
+  //       $(this).parents('.dropdown').find('.dropdown-content').show();
+  //     });
+  //
+  //     $(document).touchstart(function () {
+  //       ('.dropdown-content').hide();
+  //     });
+  //   }
+  // };
 
-      $('.dropbtn.is-active').once('shopDropdownBtn').touchstart(function () {
-        $(this).parents('.dropdown').find('.dropdown-content').show();
-      });
-
-      $(document).touchstart(function () {
-        ('.dropdown-content').hide();
-      });
-    }
-  };
+  // /**
+  //  *
+  //  */
+  // Drupal.behaviors.shopDropdownLang = {
+  //   attach: function (context, settings) {
+  //
+  //     $('#block-languageswitcher.is-active').once('shopDropdownLang').each(function () {
+  //
+  //       $(this).parents('.dropdown').find('.dropbtn').append($(this));
+  //       $(this).hide();
+  //     });
+  //   }
+  // };
 
 })(Drupal, jQuery);
