d0309fe1307e25e151589254e2410b663a71bc3a
Ivan Berezhnov <ivan.berezhnov@icloud.com>
2019-05-02 23:43:13 +0300

    save

--- docroot/themes/custom/shops/js/global-menu.js
+++ docroot/themes/custom/shops/js/global-menu.js
@@ -4,42 +4,96 @@
 
 (function (Drupal, $) {
   'use strict';
-  //
-  // /**
-  //  * Show mobile menu.
-  //  */
-  // Drupal.behaviors.mobileMenuOpen = {
-  //   attach: function (context, settings) {
-  //     $('.menu--mobile').once('mobile-menu-show').on('click', function() {
-  //       $('.mobile-navigation').toggleClass('show');
-  //       $('.overlay').toggle();
-  //     });
-  //   }
-  // };
-  //
-  // /**
-  //  * Hide mobile menu.
-  //  */
-  // Drupal.behaviors.mobileMenuClose = {
-  //   attach: function (context, settings) {
-  //     $('.menu--mobile__close').once('mobile-menu-close').on('click', function() {
-  //       $('.mobile-navigation').removeClass('show');
-  //       $('.overlay').toggle();
-  //     });
-  //   }
-  // };
 
   /**
    * Swipe mobile menu.
    */
-  Drupal.behaviors.mobileMenuSwipe = {
+  Drupal.behaviors.swipeToggle = {
     attach: function (context, settings) {
-      Drupal.menuSwipe();
+      $("[data-toggle]").click(function () {
+        var toggle_el = $(this).data("toggle");
+        $(toggle_el).toggleClass("open-sidebar");
+      });
     }
   };
 
-  Drupal.menuSwipe = function () {
-    $('.mobile-navigation').slideAndSwipe();
+  Drupal.behaviors.swipeInit = {
+    attach: function (context, settings) {
+      $(".swipe-area").swipe({
+        swipeStatus: function (
+          event,
+          phase,
+          direction,
+          distance,
+          duration,
+          fingers
+        ) {
+          if (phase == "move" && direction == "right") {
+            $(".container").addClass("open-sidebar");
+            return false;
+          }
+          if (phase == "move" && direction == "left") {
+            $(".container").removeClass("open-sidebar");
+            return false;
+          }
+        }
+      });
+    }
+  };
+
+  /**
+   * Logic for mobile multilevel menu.
+   */
+  Drupal.mobileMenuInit = function (state) {
+    // Toggling the hamburger nav class
+    $('.mobile-menu').toggleClass('active');
+    // Adding Class to the body to slide the menu in.
+    $('body').toggleClass('menu-active');
+    // Removing any active classes from the children menu triggers to 'reset" the menu
+    $('.mobile-menu-trigger--sub').removeClass('active');
+  };
+
+  Drupal.mobileMenuChildInit = function (state) {
+    // adding the active class when clicked.
+    state.addClass('active');
+    // adds a class to anything that wasn't toggled active:
+    // todo: remove hidden classes when clicking back + toggling hamburger nav
+    state.closest('ul').find('.active').closest('li').siblings().addClass('hidden');
+  };
+
+  Drupal.mobileMenuBack = function (state) {
+    // finding the closest ul
+    var closestActive = state.closest('ul').siblings();
+    // removing the active class to go back to the previous menu
+    closestActive.removeClass('active');
+  };
+
+
+  Drupal.behaviors.mobileMenuInit = {
+    attach: function (context, settings) {
+      $('#mobile-menu-trigger').click(function(e){
+        e.preventDefault();
+        mobileMenuInit();
+      });
+    }
+  };
+
+  Drupal.behaviors.mobileMenuChildInit = {
+    attach: function (context, settings) {
+      $('.mobile-menu-trigger--sub').click(function(e){
+        var clickedElement = $(this);
+        mobileMenuChildInit(clickedElement);
+      });
+    }
+  };
+
+  Drupal.behaviors.mobileMenuItemBack = {
+    attach: function (context, settings) {
+      $('.mobile-menu--back').click(function(e){
+        var clickedElement = $(this);
+        mobileMenuBack(clickedElement);
+      });
+    }
   };
 
 })(Drupal, jQuery);
